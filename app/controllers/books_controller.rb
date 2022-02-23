7c18bf36e7dbfa2b320820081519b17deb44cd97
EC2 Default User <ec2-user@ip-172-31-5-25.ec2.internal>
2022-02-23 09:39:54 +0000

    [add]課題３実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,10 +21,17 @@ class BooksController < ApplicationController
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(books_params)
+    redirect_to book_path(@book.id)
+    else
+    @books = Book.find(params[:id])
+    render :edit
+    end
   end
 
   def create
-    @book = Book.new
+    @book = Book.new(books_params)
     @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book.id)
@@ -34,11 +41,18 @@ class BooksController < ApplicationController
 
   end
 
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+
+  end
+
 
 private
 
   def books_params
-    params.require(:books).permit(:title, :body, :user_id)
+    params.require(:book).permit(:title, :body)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,13 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
 private
